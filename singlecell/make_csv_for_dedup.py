98f803a1bc655642a5d9d05a897ba11fa354cbba
Zhongxiao Li <liz0f@kw60413.kaust.edu.sa>
2021-09-05 14:50:33 +0300

    added arguments for make_csv_for_dedup.py

--- singlecell/make_csv_for_dedup.py
+++ singlecell/make_csv_for_dedup.py
@@ -8,19 +8,20 @@ OUTPUT: dedup.info.csv
 import os, re
 from Bio import SeqIO
 from Bio.Seq import Seq
+import sys
 
 rex = re.compile('(\S+) full_length_coverage=(\d+);length=(\d+);XM=(\S+);XC=(\S+)')
 rex_umi_only = re.compile('(\S+) full_length_coverage=(\d+);length=(\d+);XM=(\S+)')
-
-reader = SeqIO.parse(open('dedup.fasta'),'fasta')
-f = open('dedup.info.csv', 'w')
+sample_name=sys.argv[1]
+reader = SeqIO.parse(open('%s.dedup.fasta'%(sample_name)),'fasta')
+f = open('%s.dedup.info.csv'%(sample_name), 'w')
 f.write("id\tUMI\tUMIrev\tBC\tBCrev\tlength\tcount\n")
 for r in reader:
     m = rex.match(r.description)
     if m is not None:
         _id, _count, _len, _umi, _bc = m.groups()
         f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(_id,_umi,Seq(_umi).reverse_complement(),_bc,Seq(_bc).reverse_complement(),_len,_count))
-    else: 
+    else:
         m = rex_umi_only.match(r.description)
         _id, _count, _len, _umi = m.groups()
         _bc = 'NA'
